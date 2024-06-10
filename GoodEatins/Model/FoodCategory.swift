3a4567fcc35dbc1c7f21c91172475e301e3b8a1e
Anup Saud <anupsaud10@gmail.com>
2024-06-10 19:09:37 -0400

    Add foodCategory, DataSet, and Recipe models; initial uKit UI implementation

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,13 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Anup Saud on 2024-06-10.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title:String
+    let imageName:String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Anup Saud on 2024-06-10.
+//
+
+import Foundation
+
+struct Recipe{
+    var title:String
+    var instructions:String
+    var imageName:String
+}
